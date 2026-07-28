89bc288324e238a0e71f2af6a0515d6d5aa58e42
nhooven <nathan.d.hooven@gmail.com>
2026-07-28 13:22:15 -0700

    Continued to work on nice example maps

--- FIG - Example map.R
+++ FIG - Example map.R
@@ -4,7 +4,7 @@
 # EMAIL: nathan.d.hooven@gmail.com
 # BEGAN: 27 Jul 2026
 # COMPLETED: 
-# LAST MODIFIED: 27 Jul 2026
+# LAST MODIFIED: 28 Jul 2026
 # R VERSION: 4.5.2
 
 # ______________________________________________________________________________
@@ -17,6 +17,7 @@ library(tidyterra)
 library(sf)
 library(cowplot)
 library(spatialEco)
+library(ggspatial)       # scale bar
 
 # ______________________________________________________________________________
 # 2. Read in data ----
@@ -38,12 +39,21 @@ pct.units <- st_read(paste0(spat.dir, "Units/units_fixed_utm/units_fixed_utm.shp
 piles <- st_read(paste0(spat.dir, "Ground-truthing/all_piles.shp"))
 ret <- st_read(paste0(spat.dir, "Ground-truthing/all_refugia.shp"))
 
+# dissolved polygons
+poly.dh <- st_read(paste0(spat.dir, "hand_digitization/dissolved_poly/2a_DhY_dis.shp"))
+poly.om <- st_read(paste0(spat.dir, "hand_digitization/dissolved_poly/4_OM_dis.shp"))
+poly.msm <- st_read(paste0(spat.dir, "hand_digitization/dissolved_poly/6_MsM_dis.shp"))
+poly.jsm <- st_read(paste0(spat.dir, "hand_digitization/dissolved_poly/5_JsM_dis.shp"))
+
+# relocations
+pts <- readRDS("D:/hare_project/data_analysis/General/hare-gps-processing-new/data_cleaned/use_background.rds")
+
 # ______________________________________________________________________________
 # 2c. Raster data ----
 # ______________________________________________________________________________
 
-cover <- rast(paste0(spat.dir, "Rasters/cover_type/cover_type_pre.tif"))
-vo <- rast(paste0(spat.dir, "Rasters/veg_pred/RF/vo_post_new.tif"))
+vo.pre <- rast(paste0(spat.dir, "Rasters/veg_pred/RF/vo_pre_new.tif"))
+vo.post <- rast(paste0(spat.dir, "Rasters/veg_pred/RF/vo_post_new.tif"))
 
 # ______________________________________________________________________________
 # 3. Coordinate reference systems ----
@@ -74,14 +84,40 @@ wr.ret <- ret |> select(name, geometry) |>
   
   filter(unit == "1A")
 
+# relocations
+pts.1 <- pts |> 
+  
+  filter(case == 1 &
+           site %in% c("1A", "1B", "1C") & 
+           season == "off" &
+           year %in% c("POST1", "POST2")) |>
+  
+  st_as_sf(coords = c("x", "y"),
+           crs = crs.utm)
+
 # ______________________________________________________________________________
 # 5. Bounding box and crop rasters ----
 # ______________________________________________________________________________
 
 wr.bbox <- wr.units |> bbox_poly() |> st_buffer(dist = 100) |> bbox_poly()
 
-cover.crop <- cover |> crop(wr.bbox)
-vo.crop <- vo |> crop(wr.bbox)
+vo.pre.crop <- vo.pre |> crop(wr.bbox)
+vo.post.crop <- vo.post |> crop(wr.bbox)
+
+# transform to UTM and return only those within the bbox
+poly.dh <- poly.dh |> st_transform(crs = crs.utm) |> st_intersection(wr.bbox)
+poly.om <- poly.om |> st_transform(crs = crs.utm) |> st_intersection(wr.bbox)
+poly.msm <- poly.msm |> st_transform(crs = crs.utm) |> st_intersection(wr.bbox)
+poly.jsm <- poly.jsm |> st_transform(crs = crs.utm) |> st_intersection(wr.bbox)
+
+# intersect
+pts.1 <- pts.1 |> 
+  
+  # add coordinates
+  mutate(x = st_coordinates(pts.1)[, 1],
+         y = st_coordinates(pts.1)[, 2]) |>
+  
+  st_intersection(wr.bbox)
 
 # ______________________________________________________________________________
 # 6. Plots ----
@@ -93,12 +129,13 @@ ggplot() +
   
   theme_bw() +
   
-  geom_spatraster(data = vo.crop) +
+  geom_spatraster(data = vo.post.crop) +
   
   # unit boundaries
   geom_sf(data = wr.units,
           fill = NA,
-          color = "white") +
+          color = "white",
+          linewidth = 0.75) +
   
   # piles
   geom_sf(data = wr.piles,
@@ -110,8 +147,31 @@ ggplot() +
           fill = NA,
           color = "white") +
   
+  # relocations
+  geom_path(data = pts.1,
+            aes(x = x,
+                y = y,
+                group = track_season_post),
+          color = "black",
+          alpha = 0.5,
+          linewidth = 0.2) +
+  
   # colors
   scale_fill_viridis_c() +
+  
+  # legend title
+  labs(fill = "Visual obstruction") +
+  
+  # scale bar
+  annotation_scale(aes(style = "ticks"),
+                   location = "bl",
+                   line_width = 0.5,
+                   height = unit(0.1, "cm"),
+                   text_cex = 0.6,
+                   pad_x = unit(0.7, "cm"),
+                   pad_y = unit(0.2, "cm"),
+                   line_col = "black",
+                   text_col = "black") +
   
   # theme
   theme(panel.grid = element_blank(),
@@ -121,38 +181,157 @@ ggplot() +
         
         # legend
         legend.position = "inside",
-        legend.position.inside = c(0.7, 0.2),
-        legend.direction = "horizontal") -> map.base
+        legend.position.inside = c(0.83, 0.15),
+        legend.direction = "horizontal",
+        legend.background = element_rect(fill = NA),
+        legend.frame = element_rect(color = "white"),
+        legend.text = element_text(color = "white",
+                                   face = "bold"),
+        legend.text.position = "bottom",
+        legend.title = element_text(color = "white",
+                                    hjust = 0.5),
+        legend.title.position = "top",
+        
+        axis.title = element_blank()) -> map.base
+
+map.base
 
 # ______________________________________________________________________________
 # 6b. Doghair (pre-thinned) ----
-
-cover.dh <- cover.crop |> classify(rcl = matrix(data = c(1, NA,
-                                                         2, 1,
-                                                         3, NA,
-                                                         4, NA,
-                                                         5, NA,
-                                                         6, NA,
-                                                         7, NA,
-                                                         8, NA),
-                                                byrow = T,
-                                                nrow = 8))
-
 # ______________________________________________________________________________
 
 ggplot() +
   
   theme_bw() +
   
-  geom_spatraster(data = cover.dh) +
+  # basemap
+  geom_spatraster(data = vo.pre.crop) +
   
-  # color
-  scale_fill_continuous(palette = c("green3", "green3"),
-                        na.value = "gray90") +
+  # gray out the basemap
+  geom_sf(data = st_difference(st_buffer(wr.bbox, 5), poly.dh),
+          alpha = 0.65,
+          color = NA) +
+  
+  geom_sf(data = poly.dh,
+          color = "white",
+          linewidth = 0.2,
+          fill = NA) +
   
   # theme
   theme(panel.grid = element_blank(),
         panel.border = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
-        legend.position = "none")
+        legend.position = "none",
+        plot.margin = margin(0, 0, 0, 0)) +
+  
+  # colors
+  scale_fill_viridis_c() -> map.dh
+
+# ______________________________________________________________________________
+# 6c. Open mature ----
+# ______________________________________________________________________________
+
+ggplot() +
+  
+  theme_bw() +
+  
+  # basemap
+  geom_spatraster(data = vo.pre.crop) +
+  
+  # gray out the basemap
+  geom_sf(data = st_difference(st_buffer(wr.bbox, 5), poly.om),
+          alpha = 0.65,
+          color = NA) +
+  
+  geom_sf(data = poly.om,
+          color = "white",
+          linewidth = 0.2,
+          fill = NA) +
+  
+  # theme
+  theme(panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none",
+        plot.margin = margin(0, 0, 0, 0)) +
+  
+  # colors
+  scale_fill_viridis_c() -> map.om
+
+# ______________________________________________________________________________
+# 6d. Dense mature ----
+# ______________________________________________________________________________
+
+ggplot() +
+  
+  theme_bw() +
+  
+  # basemap
+  geom_spatraster(data = vo.pre.crop) +
+  
+  # gray out the basemap
+  geom_sf(data = st_difference(st_buffer(wr.bbox, 5), poly.msm),
+          alpha = 0.65,
+          color = NA) +
+  
+  geom_sf(data = poly.msm,
+          color = "white",
+          linewidth = 0.2,
+          fill = NA) +
+  
+  # theme
+  theme(panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none",
+        plot.margin = margin(0, 0, 0, 0)) +
+  
+  # colors
+  scale_fill_viridis_c() -> map.msm
+
+# ______________________________________________________________________________
+# 6e. Jackstraw ----
+# ______________________________________________________________________________
+
+ggplot() +
+  
+  theme_bw() +
+  
+  # basemap
+  geom_spatraster(data = vo.pre.crop) +
+  
+  # gray out the basemap
+  geom_sf(data = st_difference(st_buffer(wr.bbox, 5), poly.jsm),
+          alpha = 0.65,
+          color = NA) +
+  
+  geom_sf(data = poly.jsm,
+          color = "white",
+          linewidth = 0.2,
+          fill = NA) +
+  
+  # theme
+  theme(panel.grid = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "none",
+        plot.margin = margin(0, 0, 0, 0)) +
+  
+  # colors
+  scale_fill_viridis_c() -> map.jsm
+
+# ______________________________________________________________________________
+# 6e. Plot together ----
+# ______________________________________________________________________________
+
+maps.cover <- plot_grid(map.dh, map.msm, map.om, map.jsm, 
+                        ncol = 1,
+                        align = "h",
+                        axis = "l",
+                        rel_heights = c(1.5, 1, 1, 1))
+
+maps.cover
